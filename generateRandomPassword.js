7194480163af3436b5ce49c6fb32fc2860b7a744
Manik Rastogi <Manik3112@gmail.com>
2022-10-26 12:51:15 +0530

    Create generateRandomPassword.js

---
+++ generateRandomPassword.js
@@ -0,0 +1,13 @@
+// Generate a Random Password using the Alpha Numeric
+function generateRandomPassword(length){
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    let retVal = "";
+    for (let i = 0, n = charset.length; i < length; ++i) {
+        retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    return retVal;
+}
+
+// Execution 
+// INPUT => generateRandomPassword(8);
+// Output => i52Ht7tb
